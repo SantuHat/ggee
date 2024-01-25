a27038a881a9f003f12ca2cb9459679b280f95a2
santu <j6458221@yahoo.com.tw>
2024-01-25 18:30:21 +0800

    驗證登入成功後取得產品資料

--- products.js
+++ products.js
@@ -1,1 +1,46 @@
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
+
+createApp({
+  data() {
+    return {
+      apiUrl: `https://ec-course-api.hexschool.io/v2`,
+      apiPath: "santu",
+      products: [],
+      tempProduct: {},
+    };
+  },
+  methods: {
+    // 驗證登入
+    checkAdmin() {
+      axios
+        .post(`${this.apiUrl}/api/user/check`)
+        .then(() => {
+          this.getData();
+        })
+        .catch((error) => {
+          alert(error.response.data.message);
+        });
+    },
+    getData() {
+      axios
+        .get(`${this.apiUrl}/api/${this.apiPath}/admin/products`)
+        .then((res) => {
+          console.log(res.data.products);
+        })
+        .catch((error) => {
+          alert(error.response.data.message);
+        });
+    },
+  },
+  mounted() {
+    // 取出 Token
+    const token = document.cookie.replace(
+      // 須與瀏覽器中的key一致
+      /(?:(?:^|.*;\s*)week2Token\s*=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    );
+    axios.defaults.headers.common.Authorization = token;
+
+    this.checkAdmin();
+  },
+}).mount("#app");
